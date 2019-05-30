37427483541eadad35119163a2af2f7c0fb2947b
Juan Nadar Yanac Barreto <juan.yanac@outlook.com>
2019-05-29 21:40:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/jyanac/workshopmongo/resource/UserResource.java
+++ src/main/java/com/jyanac/workshopmongo/resource/UserResource.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class UserResource {
 	
 	//@GetMapping
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> finALl()
+	public ResponseEntity<List<UserDTO>> findALl()
 	{
 		//User maria = new User("1","Maria Brown","maria@gmail.com");
 		//User alex = new User("2","Alex Green","alex@gmail.com");
@@ -35,4 +36,11 @@ public class UserResource {
 		List<UserDTO> listDTO = list.stream().map(x->new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id)
+	{
+		User obj = service.finById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}	
 }
--- src/main/java/com/jyanac/workshopmongo/services/UserService.java
+++ src/main/java/com/jyanac/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jyanac.workshopmongo.services;
 
-import java.util.List;import javax.jws.soap.SOAPBinding.Use;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jyanac.workshopmongo.domain.User;
 import com.jyanac.workshopmongo.repository.UserRepository;
+import com.jyanac.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll()
 	{
 		return repo.findAll(); 
+	}
+	public User finById(String id)
+	{
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
