a51c5101aed634e5561597c0b157b1121346ed7b
Juan Nadar Yanac Barreto <juan.yanac@outlook.com>
2019-05-31 01:05:44 -0300

    Obtendo um post por id

--- src/main/java/com/jyanac/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jyanac/workshopmongo/config/Instantiation.java
@@ -2,7 +2,6 @@ package com.jyanac.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/jyanac/workshopmongo/resource/UserResource.java
+++ src/main/java/com/jyanac/workshopmongo/resource/UserResource.java
@@ -1,16 +1,12 @@
 package com.jyanac.workshopmongo.resource;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
