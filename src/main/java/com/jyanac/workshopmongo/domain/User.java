4a54993172ce22870905daf52ed7a8b4ad02fa40
Juan Nadar Yanac Barreto <juan.yanac@outlook.com>
2019-05-31 00:46:08 -0300

    Referenciando os posts do usuário

--- src/main/java/com/jyanac/workshopmongo/domain/User.java
+++ src/main/java/com/jyanac/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.jyanac.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -14,12 +17,16 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
-	public User(String id, String email, String name) {
+	public User(String id, String name,String email) {
+		super();
 		this.id = id;
-		this.email = email;
 		this.name = name;
+		this.email = email;
 	}
 	public String getId() {
 		return id;
@@ -39,6 +46,13 @@ public class User implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
